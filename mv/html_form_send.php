dabd77d9b2ad6f61450191008c48fe7c7c396d5c
EngineerEmily <Emily00@gmail.com>
2015-09-23 18:20:41 -0700

    got email to work

--- mv/html_form_send.php
+++ mv/html_form_send.php
@@ -1,80 +1,51 @@
+<html>
+<body>
+
 <?php
-if(isset($_POST['email'])) {
-     
-    // CHANGE THE TWO LINES BELOW
-    $email_to = "Emily00@gmail.com";
-     
-    $email_subject = "New Member";
-     
-     
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
-     
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
-     
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
-     
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-    $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-    $email_message = "Form details below.\n\n";
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-     
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
-     
-     
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
+$first_name = $_POST["first_name"];
+$last_name = $_POST["last_name"];
+$address = $_POST["address"];
+$city = $_POST["city"];
+$state = $_POST["state"];
+$zip = $_POST["zip"];
+$telephone = $_POST["telephone"];
+$email = $_POST["email"];
+$membership_type = $_POST["membership_type"];
+$amount_enclosed = $_POST["amount_enclosed"];
+$to = "membership@mountainviewhistorical.org";
+$subject = "MVHA New Member";
+$message = "First Name: " . $first_name . "<br>" . "Last Name: " . $last_name . "<br>" . "Address: " . $address . "<br>" . "City: " . $city . "<br>" . "State: " . $state . "<br>" . "Zip: " . $zip . "<br>" . "Telephone: " . $telephone . "<br>" . "Email: " . $email . "<br>" . "Membership Type: " . $membership_type . "<br>" . "Amount Enclosed $" . $amount_enclosed . "<br>"; 
+$headers = 'From: membership@mountainviewhistorical.org' . "\r\n" .
+'Reply-To: membership@mountainviewhistorical.org' . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+
+mail ($to, $subject, $message, $headers);
+echo "Thank you for filling out the membership form <a href='http://google.com' target=_blank'>click here</a> to pay your membership dues online<br><br>";
+echo "Paying by check? Print out this page and send it with the check to:<br><br>";
+echo "<b>Mountain View Historical Association</b><br>";
+echo "P. O. Box 252 <br>";
+echo "Mountain View, CA 94042 <br><br>";
+
+echo "First Name: ";
+echo $first_name . "<br>";
+echo "Last Name: ";
+echo $last_name . "<br>";
+echo "Address: ";
+echo $address . "<br>";
+echo "City: ";
+echo $city . "<br>";
+echo "State: ";
+echo $state . "<br>";
+echo "Zip: ";
+echo $zip . "<br>";
+echo "Telephone: ";
+echo $telephone . "<br>";
+echo "Email: ";
+echo $email . "<br>";
+echo "Membership Type: ";
+echo $membership_type . "<br>";
+echo "Amount Enclosed $";
+echo $amount_enclosed . "<br>";
 ?>
- 
-<!-- place your own success html below -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
-}
-die();
-?>
+</body>
+</html>
