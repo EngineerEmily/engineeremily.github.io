dd5a557c7aac01b68a39c5811d78da229a4de78c
EngineerEmily <Emily00@gmail.com>
2015-09-23 19:01:54 -0700

    grrrr headers why won't you work?

--- mv/html_form_send.php
+++ mv/html_form_send.php
@@ -12,15 +12,15 @@ $telephone = $_POST["telephone"];
 $email = $_POST["email"];
 $membership_type = $_POST["membership_type"];
 $amount_enclosed = $_POST["amount_enclosed"];
-$to = "membership@mountainviewhistorical.org";
+$to = "Emily00@gmail.com";
 $subject = "MVHA New Member";
 $message = "First Name: " . $first_name . "<br>" . "Last Name: " . $last_name . "<br>" . "Address: " . $address . "<br>" . "City: " . $city . "<br>" . "State: " . $state . "<br>" . "Zip: " . $zip . "<br>" . "Telephone: " . $telephone . "<br>" . "Email: " . $email . "<br>" . "Membership Type: " . $membership_type . "<br>" . "Amount Enclosed $" . $amount_enclosed . "<br>"; 
-$headers = 'From: membership@mountainviewhistorical.org' . "\r\n" .
-'Reply-To: membership@mountainviewhistorical.org' . "\r\n" .
-'X-Mailer: PHP/' . phpversion();
 
-mail ($to, $subject, $message, $headers);
-echo "Thank you for filling out the membership form <a href='http://google.com' target=_blank'>click here</a> to pay your membership dues online<br><br>";
+// More headers
+$headers = $email;
+
+mail ($to,$subject,$message,$headers);
+echo "Thank you for filling out the membership form <a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CPX79245TXTYE' target=_blank'>click here</a> to pay your membership dues online<br><br>";
 echo "Paying by check? Print out this page and send it with the check to:<br><br>";
 echo "<b>Mountain View Historical Association</b><br>";
 echo "P. O. Box 252 <br>";
